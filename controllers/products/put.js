24d731e4e744fa0a9ce6f3e1a12eb65937e02120
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-11-24 19:59:26 -0300

    Requisito 3

--- controllers/products/put.js
+++ controllers/products/put.js
@@ -1,13 +1,33 @@
 const model = require('../../model/products');
+const validations = require('../../service/products/validations');
+
+const runValidations = (name, quantity, object) => {
+  if (!validations.nameLengthValidation(name)) {
+    return '"name" length must be at least 5 characters long';
+  }
+  if (!validations.quantityValueValidation(quantity)) {
+    return '"quantity" must be larger than or equal to 1';
+  }
+  if (!validations.quantityTypeValidation(quantity)) {
+    return '"quantity" must be a number';
+  }
+  if (!object) {
+    return 'Wrong id format';
+  }
+  return null;
+};
 
 module.exports = async (req, res, next) => {
+  const { id } = req.params;
+  const payload = req.body;
+  const { name, quantity } = payload;
+  const object = await model.findById(id);
+  const validationReturn = runValidations(name, quantity, object);
   try {
-    const { id } = req.params;
-    const object = await model.findById(id);
-    if (!object) {
+    if (validationReturn) {
       return res.status(422).send({ err: {
         code: 'invalid_data',
-        message: 'Wrong id format',
+        message: validationReturn,
       },
       });
     }
