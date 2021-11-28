7430fa6cf394800c8b40dc325ae9c65685e7afc4
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-11-28 19:33:45 -0300

    Requisito 7

--- controllers/sales/put.js
+++ controllers/sales/put.js
@@ -1,17 +1,29 @@
 const model = require('../../model/sales');
+const validations = require('../../service/sales/validations');
+
+const runValidations = (payload) => {
+  if (!validations.quantityValueValidation(payload)) {
+    return 'Wrong product ID or invalid quantity';
+  }
+  if (!validations.quantityTypeValidation(payload)) {
+    return 'Wrong product ID or invalid quantity';
+  }
+  return null;
+};
 
 module.exports = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const object = await model.findById(id);
-    if (!object) {
+    const payload = req.body;
+    const validationReturn = runValidations(payload);
+    if (validationReturn) {
       return res.status(422).send({ err: {
         code: 'invalid_data',
-        message: 'Wrong id format',
+        message: validationReturn,
       },
       });
     }
-    const updatedObject = await model.update({ ...object, ...req.body });
+    const updatedObject = await model.update({ _id: id, itensSold: payload });
     return res.status(200).send(updatedObject);
   } catch (err) {
     next(err);
